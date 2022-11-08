143fd79d81ca522352cbd5f12da6cc263c907a29
Tribhuwan kumar <107320592+tribanj@users.noreply.github.com>
2022-11-09 00:22:42 +0530

    Add files via upload

---
+++ new.java
@@ -0,0 +1,29 @@
+// class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!"); 
+//     }
+// }
+class Number{
+    int x;
+    public Number(int i)
+    {
+        n=i;
+    }
+
+}
+class summer{
+    public void swap(Number n1; Number n2){
+        int t ;
+        t=n1.x;
+        n1.x=n2.x;
+        n2.x=t;
+    }
+    public void swap (int x1, int x2){
+        int t;
+        t=n;
+        x1=x2;
+        x2=t;
+    }
+}
+s.swap1(x1,x2);
+s.swap2(x1.x,x2.x);
